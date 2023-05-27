47e4d2319ae37926feaf5e28b3a69c42829ad7ec
walvy <geometrydashlove44@gmai.com>
2023-05-27 20:44:07 +0300

    add background

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import pygame
 import os
 pygame.init()
 
-
 def file_path(file_name):
     folder = os.path.abspath(__file__+ "/..")
     path = os.path.join(folder, file_name)
+    return path
 
 
 WIN_WIDTH = 900
 WIN_HEIGHT = 600
 FPS = 40
+
+fon = pygame.image.load(file_path(r"images\saturn.jpg"))
+fon = pygame.transform.scale(fon, (WIN_WIDTH, WIN_HEIGHT))
+
 
 window =  pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 clock = pygame.time.Clock()
@@ -23,7 +27,7 @@ while game:
             game = False
 
     if level == 1:
-        pass
+        window.blit(fon, (0, 0))
 
     clock.tick(FPS)
     pygame.display.update()
