cc3a9c4623f26bab9817e0e8ed2006bb5db193f9
walvy <geometrydashlove44@gmai.com>
2023-06-10 20:01:50 +0300

    add player, enemy, finish

--- main.py
+++ main.py
@@ -12,12 +12,27 @@ WIN_WIDTH = 900
 WIN_HEIGHT = 600
 FPS = 40
 
-fon = pygame.image.load(file_path(r"images\saturn.jpg"))
+fon = pygame.image.load(file_path(r"images\space.jpg"))
 fon = pygame.transform.scale(fon, (WIN_WIDTH, WIN_HEIGHT))
 
 
 window =  pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 clock = pygame.time.Clock()
+
+
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, image):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.image.load(file_path(image))
+        self.image = pygame.transform.scale(self.image, (width, height))
+    def show(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+player = GameSprite(50, 50, 20, 30, r"images\player.jpg") 
+enemy =  GameSprite(50, 50, 30, 20, r"images\enemy.jpg")
+finish = GameSprite(50, 50 , 30, 20, r"images\goal.jpg")
+
+
 
 level = 1
 game = True
@@ -28,6 +43,9 @@ while game:
 
     if level == 1:
         window.blit(fon, (0, 0))
+        player.show()
+        enemy.show()
+        finish.show()
 
     clock.tick(FPS)
     pygame.display.update()
